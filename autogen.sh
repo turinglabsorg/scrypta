f0b0cd9303d491dc739efa952d80199dec71c904
Travis Finch <travisfinch01@gmail.com>
2020-10-14 16:03:38 -0400

    test

--- autogen.sh
+++ autogen.sh
@@ -3,8 +3,9 @@ srcdir="$(dirname $0)"
 cd "$srcdir"
 if [ -z ${LIBTOOLIZE} ] && GLIBTOOLIZE="`which glibtoolize 2>/dev/null`"; then
   LIBTOOLIZE="${GLIBTOOLIZE}"
+  libtoolize--automake--copy--debug--force
   export LIBTOOLIZE
 fi
 which autoreconf >/dev/null || \
   (echo "configuration failed, please install autoconf first" && exit 1)
-autoreconf -i
+autoreconf --install --force --warnings=all
