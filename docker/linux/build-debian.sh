398f27eba79bead02f3cafda84acae5d2294d0a6
turinglabsorg <s.cataudo@turinglabs.org>
2020-11-26 11:58:35 +0100

    ubuntu 16 working docker

--- docker/linux/build-debian.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-cd debian
-mkdir build
-docker build -t scrypta:debian .
-docker run -d --name debian -p 42222:42222 -p 42223:42223 scrypta:debian
-docker cp debian:/opt/scrypta/src/lyrad  ./build/lyrad
-docker cp debian:/opt/scrypta/src/lyra-cli  ./build/lyra-cli
-docker cp debian:/opt/scrypta/src/qt/lyra-qt  ./build/lyra-qt
--- docker/linux/build-ubuntu16.sh
+++ docker/linux/build-ubuntu16.sh
@@ -2,6 +2,7 @@
 
 cd ubuntu-16
 mkdir build
+chmod 777 build
 docker build -t scrypta:ubuntu16 .
 docker run -d --name ubuntu16 -p 42222:42222 -p 42223:42223 scrypta:ubuntu16
 docker cp ubuntu16:/opt/scrypta/src/lyrad  ./build/lyrad
