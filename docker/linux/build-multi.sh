0f0fdd2655501d573c5968debfdcf74d95bda3ba
turinglabsorg <s.cataudo@turinglabs.org>
2020-11-27 14:05:34 +0100

    dev docker and ui fix

--- docker/linux/build-multi.sh
+++ docker/linux/build-multi.sh
@@ -3,7 +3,7 @@
 cd multi
 mkdir build
 docker build -t scrypta:multi .
-docker run -d --name multi -p 42222:42222 -p 42223:42223 scrypta:multi
+docker run -d --name multi scrypta:multi
 docker cp multi:/opt/scrypta/src/lyrad  ./build/lyrad
 docker cp multi:/opt/scrypta/src/lyra-cli  ./build/lyra-cli
 docker cp multi:/opt/scrypta/src/qt/lyra-qt  ./build/lyra-qt
--- docker/linux/dev.sh
+++ docker/linux/dev.sh
@@ -1,9 +1,16 @@
 #!/bin/bash
 
+rm -rf dev
 mkdir dev
 cd dev
 
-docker exec -d ubuntu18 cd /opt/scrypta/ && make clean && make -j16
-docker cp ubuntu18:/opt/scrypta/src/lyrad  ./lyrad
-docker cp ubuntu18:/opt/scrypta/src/lyra-cli  ./lyra-cli
-docker cp ubuntu18:/opt/scrypta/src/qt/lyra-qt  ./lyra-qt
+docker exec -it -w /opt/scrypta/ multi git pull
+docker exec -it -w /opt/scrypta/ multi make clean
+docker exec -it -w /opt/scrypta/ multi make -j16
+
+docker cp multi:/opt/scrypta/src/lyrad  ./lyrad
+docker cp multi:/opt/scrypta/src/lyra-cli  ./lyra-cli
+docker cp multi:/opt/scrypta/src/qt/lyra-qt  ./lyra-qt
+
+sleep 2
+./lyra-qt
