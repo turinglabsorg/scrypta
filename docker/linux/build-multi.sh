5cae93b36995c467ae35e58af7c720a08f8feb85
turinglabsorg <s.cataudo@turinglabs.org>
2020-11-30 09:02:34 +0100

    fixed deb creation package

--- docker/linux/build-multi.sh
+++ docker/linux/build-multi.sh
@@ -8,6 +8,8 @@ docker cp multi:/opt/scrypta/src/lyrad  ./build/lyrad
 docker cp multi:/opt/scrypta/src/lyra-cli  ./build/lyra-cli
 docker cp multi:/opt/scrypta/src/qt/lyra-qt  ./build/lyra-qt
 
+rm -rf scrypta-wallet
+rm scrypta-wallet.deb
 mkdir scrypta-wallet
 cd scrypta-wallet
 
@@ -26,8 +28,11 @@ Description: FullNode Wallet for Scrypta Blockchain" >> control
 
 cd ..
 mkdir usr
-cp ../build usr
-mv usr/build usr/bin
-
+mkdir usr/bin/
 cd ..
-dpkg-deb --build scrypta-wallet
+
+cp ./build/lyra-cli ./scrypta-wallet/usr/bin/lyra-cli
+cp ./build/lyrad ./scrypta-wallet/usr/bin/lyrad
+cp ./build/lyra-qt ./scrypta-wallet/usr/bin/lyra-qt
+
+sudo dpkg-deb --build ./scrypta-wallet
